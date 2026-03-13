e3793d2bdf2009072023df62f9aa9042741a3420
manael4 <161084862+manael4@users.noreply.github.com>
2026-03-13 17:57:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def hola():
-  return "¡Bienvenido a BurgerCode! La mejor hamburguesa v1.0 🍔"
+    # El contenido de la función debe estar indentado
+    return "¡Bienvenido a BurgerCode! La mejor hamburguesa v1.0 🍔"
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    # El bloque de ejecución también debe estar indentado
+    app.run(host='0.0.0.0', port=5000)
